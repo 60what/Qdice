2e61a8cd2ab76b83075cbe1dd6e31ef7f8f0d621
60what <103315393+60what@users.noreply.github.com>
2022-04-23 17:27:21 -0400

    Add files via upload

--- qscript.js
+++ qscript.js
@@ -512,7 +512,7 @@ function handleBackEvents() {
 	window.history.pushState({}, '');
 	window.addEventListener('popstate', () => {
 
-		simpleToast(2000, 'Double tap BACK\r\nto exit the app v1');
+		simpleToast(2000, 'Double tap BACK\r\nto exit the app v2');
 
 		setTimeout(() => {
 			window.history.pushState({}, '');
